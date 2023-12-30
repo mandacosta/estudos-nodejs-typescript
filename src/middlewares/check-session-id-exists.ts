03dc0cf01f0ebdac8ee188bae3b06b5c14dee7f2
Amanda <mandacosta94@gmail.com>
2023-12-29 21:42:56 -0300

    feat: criando o session id nos cookies

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,4 @@
+// Criar o mid que checa se tem session id
+// TIpar usando o fastfyReply e fastifyRequest
+// Incorporar no segundo parametro da rota dentro de um objeto com a chave preHandlers que é um array
+// Ir checando em todas as rotas se ta enviando apenas os dados referentes ao session id
