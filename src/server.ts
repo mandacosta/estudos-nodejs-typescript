816a3496cd942bc39a4cbdf591fac62f6fb791be
Amanda <mandacosta94@gmail.com>
2023-12-30 11:19:53 -0300

    feat: configura os modulos de teste

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,5 @@
-import fastify from 'fastify'
 import { env } from './env'
-import { transactionRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionRoutes, {
-  prefix: 'transactions',
-})
+import { app } from './app'
 
 app
   .listen({
