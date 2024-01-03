8f0abcdbee353d31a665f32e60844573bd58a1be
Amanda <mandacosta94@gmail.com>
2024-01-02 21:30:32 -0300

    feat: preparando para deploy - instalando tsup para compilação em JS

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import { Knex, knex as setupKnex } from 'knex'
 import { env } from './env'
 
-export const config: Knex = {
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: env.DATABASE_URL,
